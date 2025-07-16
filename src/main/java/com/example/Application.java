48f1a8aa58f26b5b43b757a654d47b4977182ab1
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-16 16:31:24 +0530

    Student object created

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,30 @@
 package com.example;
 
+import com.example.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping
+    public List<Student> hello(){
+        return List.of(
+                new Student(
+                        1L, "Rinos", "rinos@gmail.com", LocalDate.of(2002, Month.FEBRUARY, 21), 23
+                )
+        );
     }
 
 }
